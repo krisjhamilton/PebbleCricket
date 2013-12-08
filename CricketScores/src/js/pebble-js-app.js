cfce1d4e8d15ebb482447d0af6b4420066d7384f
Ishan Thukral <dev@ishan.me>
2013-12-08 09:28:29 -0500

    initial commit

---
+++ CricketScores/src/js/pebble-js-app.js
@@ -0,0 +1,19 @@
+var teamHash = {
+	"Australia": "AUS",
+	"Bangladesh": "BAN",
+	"England": "ENG",
+	"India": "IND",
+	"Kenya": "KEN",
+	"New Zealand": "NZ",
+	"Pakistan": "PAK",
+	"South Africa": "SA",
+	"Sri Lanka": "SL",
+	"West Indies": "WI",
+	"Zimbabwe": "ZIM"
+}
+
+Pebble.addEventListener("ready",
+    function(e) {
+        console.log("Hello world! - Sent from your javascript application.");
+    }
+);
